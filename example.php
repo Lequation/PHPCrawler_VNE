a7a0dc1196f76ee3a02ea4151ef259a8861e9640
Lequation <longvt1912@gmil.com>
2017-10-18 18:46:37 +0700

    new

--- example.php
+++ example.php
@@ -17,73 +17,78 @@ class MyCrawler extends PHPCrawler
 
         // Lấy toàn bộ url của website
         echo "Page requested: ".$DocInfo->url."\n";
-		$file = 'logs_id';
-		$contents = file_get_contents($file);
-		if(strpos($contents, $DocInfo->url) === false && strpos($contents, $DocInfo->url) === false){
-			if (strpos($DocInfo->url, "/tin-tuc/giao-duc/") !== false && strpos($DocInfo->url, "?") === false) {
-		        $html = file_get_html($DocInfo->url);
-		        if(is_object($html)){
-		            $type = $html->find("meta[name='tt_page_type']", 0)->content;
-		            $id = $html->find("meta[name='tt_article_id']", 0)->content;
-		            	if($type === "article"){
-				            $t = $html->find('.block_ads_connect', 0)->plaintext;
-				            //$t = str_replace("'", '"', $t);
-				            $t = addslashes($t);
-				            //echo trim($t)."\n";
-				            $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
-				            
-							if(strpos($contents, $id) === false){
-				 				$giaoduc = file_put_contents('logs_giaoduc', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
-								$logs_id = file_put_contents('logs_id', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
-							}
-				            $html->clear();
-				            unset($html);
-			        }
-		        }
-			}
-			if (strpos($DocInfo->url, "/tin-tuc/khoa-hoc/") !== false && strpos($DocInfo->url, "?") === false) {
-		        $html = file_get_html($DocInfo->url);
-		        if(is_object($html)){
-		            $type = $html->find("meta[name='tt_page_type']", 0)->content;
-		            $id = $html->find("meta[name='tt_article_id']", 0)->content;
-		            	if($type === "article"){
-				            $t = $html->find('.block_ads_connect', 0)->plaintext;
-				            //$t = str_replace("'", '"', $t);
-				            $t = addslashes($t);
-				            //echo trim($t)."\n";
-				            $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
-				            
-							if(strpos($contents, $id) === false){
-				 				$khoahoc = file_put_contents('logs_khoahoc', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
-								$logs_id = file_put_contents('logs_id', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
-							}
-				            $html->clear();
-				            unset($html);
-			        }
-		        }
-			}
-			if (strpos($DocInfo->url, "/tin-tuc/phap-luat/") !== false && strpos($DocInfo->url, "?") === false) {
-		        $html = file_get_html($DocInfo->url);
-		        if(is_object($html)){
-		            $type = $html->find("meta[name='tt_page_type']", 0)->content;
-		            $id = $html->find("meta[name='tt_article_id']", 0)->content;
-		            	if($type === "article"){
-				            $t = $html->find('.block_ads_connect', 0)->plaintext;
-				            //$t = str_replace("'", '"', $t);
-				            $t = addslashes($t);
-				            //echo trim($t)."\n";
-				            $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
-				            
-							if(strpos($contents, $id) === false){
-				 				$phapluat = file_put_contents('logs_phapluat', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
-								$logs_id = file_put_contents('logs_id', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
-							}
-				            $html->clear();
-				            unset($html);
-			        }
-		        }
-			}
-		}
+
+        //if(strpos($contents, $DocInfo->url) === false && strpos($contents, $DocInfo->url) === false){
+        if (strpos($DocInfo->url, "/tin-tuc/giao-duc/") !== false && strpos($DocInfo->url, "?") === false) {
+            $html = file_get_html($DocInfo->url);
+            if(is_object($html)){
+                $type = $html->find("meta[name='tt_page_type']", 0)->content;
+                $id = $html->find("meta[name='tt_article_id']", 0)->content;
+                if($type === "article"){
+                    $t = $html->find('.block_ads_connect', 0)->plaintext;
+                    //$t = str_replace("'", '"', $t);
+                    $t = addslashes($t);
+                    //echo trim($t)."\n";
+                    $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
+
+                    $file = 'logs_id_giaoduc';
+                    $contents = file_get_contents($file);
+                    if(strpos($contents, $id) === false){
+                        $giaoduc = file_put_contents('logs_giaoduc', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
+                        $logs_id = file_put_contents('logs_id_giaoduc', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
+                    }
+                    $html->clear();
+                    unset($html);
+                }
+            }
+        }
+        if (strpos($DocInfo->url, "/tin-tuc/khoa-hoc/") !== false && strpos($DocInfo->url, "?") === false) {
+            $html = file_get_html($DocInfo->url);
+            if(is_object($html)){
+                $type = $html->find("meta[name='tt_page_type']", 0)->content;
+                $id = $html->find("meta[name='tt_article_id']", 0)->content;
+                if($type === "article"){
+                    $t = $html->find('.block_ads_connect', 0)->plaintext;
+                    //$t = str_replace("'", '"', $t);
+                    $t = addslashes($t);
+                    //echo trim($t)."\n";
+                    $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
+
+                    $file = 'logs_id_khoahoc';
+                    $contents = file_get_contents($file);
+                    if(strpos($contents, $id) === false){
+                        $khoahoc = file_put_contents('logs_khoahoc', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
+                        $logs_id = file_put_contents('logs_id_khoahoc', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
+                    }
+                    $html->clear();
+                    unset($html);
+                }
+            }
+        }
+        if (strpos($DocInfo->url, "/tin-tuc/phap-luat/") !== false && strpos($DocInfo->url, "?") === false) {
+            $html = file_get_html($DocInfo->url);
+            if(is_object($html)){
+                $type = $html->find("meta[name='tt_page_type']", 0)->content;
+                $id = $html->find("meta[name='tt_article_id']", 0)->content;
+                if($type === "article"){
+                    $t = $html->find('.block_ads_connect', 0)->plaintext;
+                    //$t = str_replace("'", '"', $t);
+                    $t = addslashes($t);
+                    //echo trim($t)."\n";
+                    $t = preg_replace('/[ ]{2,}|[\t]/', '', trim($t))."\n";
+
+                    $file = 'logs_id_phapluat';
+                    $contents = file_get_contents($file);
+                    if(strpos($contents, $id) === false){
+                        $phapluat = file_put_contents('logs_phapluat', $t.PHP_EOL , FILE_APPEND | LOCK_EX);
+                        $logs_id = file_put_contents('logs_id_phapluat', $DocInfo->url."\n".$id."\n".PHP_EOL , FILE_APPEND | LOCK_EX);
+                    }
+                    $html->clear();
+                    unset($html);
+                }
+            }
+        }
+        //}
         flush();
     }
 }
@@ -101,25 +106,32 @@ $crawler->addContentTypeReceiveRule("#text/html#");
 // Một bộ lọc cho phép ta không lấy các link ảnh, css hoặc javascript
 $crawler->addURLFilterRule("#(jpg|gif|png|pdf|jpeg|svg|css|js)$# i");
 
+// Cai dat khoi dong lai
+$crawler->enableResumption();
+if (!file_exists("/tmp/example.tmp"))
+{
+    $crawler_ID = $crawler->getCrawlerId();
+    file_put_contents("/tmp/example.tmp", $crawler_ID);
+}
+else
+{
+    $crawler_ID = file_get_contents("/tmp/example.tmp");
+    $crawler->resume($crawler_ID);
+}
+
 // Trong quá trình crawler, lưu trữ và gửi cookie giống như ta vào bằng trinh duyệt
 $crawler->enableCookieHandling(true);
 
 // Thiết lập dung lượng(bytes) thu thập được trong quá trình crawler
 //$crawler->setTrafficLimit(1000 * 1024);
 
+
 // Nào, chạy thôi, hehe, :))
 $crawler->go();
+//$crawler->goMultiProcessed(5);
 
 // Sau khi quá trình crawler kết thúc, ghi lại báo cáo!!
 
-$report = $crawler->getProcessReport();
+//$report = $crawler->getProcessReport();
 
-if (PHP_SAPI == "cli") $lb = "\n";
-else $lb = "<br />";
-
-echo "Summary:".$lb;
-echo "Links followed: ".$report->links_followed.$lb;
-echo "Documents received: ".$report->files_received.$lb;
-echo "Bytes received: ".$report->bytes_received." bytes".$lb;
-echo "Process runtime: ".$report->process_runtime." sec".$lb;
 ?>
